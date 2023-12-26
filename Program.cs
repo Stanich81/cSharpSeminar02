4fb454769f8eff5937702f388127c7109ea22bbf
Stanislav <stanich81@gmail.com>
2023-12-26 22:25:00 +0300

    Добавили задачу №1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает
+// на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+Console.Clear();
+Console.WriteLine("Введите число : ");
+int n = Convert.ToInt32(Console.ReadLine()!);
+if (n % 7 == 0 && n % 23 == 0)
+{
+    Console.WriteLine($"Введенное число {n} одновременно кратно числам 7 и 23");
+}
+else 
+{
+    Console.WriteLine($"Введенное число {n} НЕ кратно числам 7 и 23");
+}
